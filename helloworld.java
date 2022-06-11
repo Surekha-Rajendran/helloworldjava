fd744ae55af35cfc185f0ba3da72db2f24b9e957
Surekha-Rajendran <106362875+Surekha-Rajendran@users.noreply.github.com>
2022-06-11 20:13:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 11th june - webhooks demo1!");
+    System.out.println("Hello World 11th june - webhooks demo!");
     }
 }
